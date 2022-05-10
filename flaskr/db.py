953ddf8a093ba27faca2f4fad26a381fcabbb836
Stefano <sena.stefano@gmail.com>
2022-05-10 10:36:39 -0400

    added funcs that will run the sql

--- flaskr/db.py
+++ flaskr/db.py
@@ -39,3 +39,23 @@ def close_db(e=None):
         db.close()
 
 
+#add the funcs that will run the SQL command
+
+def init_db():
+
+    #gets and returns the database connection
+    db = get_db()
+
+    #open)resource() opens a file relative to the flaskr package 
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    #Clear the existing data and create new tables.
+    init_db()
+    click.echo('Initialized the database.')
+
+
